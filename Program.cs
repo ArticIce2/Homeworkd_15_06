e1fa4bd20cd85178b63c4017da6d5d794b66a755
Artic <artic.blue17@mail.ru>
2023-06-15 03:00:07 +0300

    Homeworkd 15_06

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+//Задание 1
+// System.Console.Write("Введите трёхзначное число -> ");
+// int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+// string stringNumber = Convert.ToString(threeDigitNumber);
+// System.Console.WriteLine($"Вторая цифра трёхначного числа ->"+stringNumber[1]);
+
+//Задание 2
+ 
+// System.Console.Write("Введите  число -> ");
+// int anyNumber = Convert.ToInt32(Console.ReadLine());
+// string anyNumberOne = Convert.ToString(anyNumber);
+// if(anyNumberOne.Length > 2)
+// {
+//     System.Console.WriteLine($"Третье число ->"+anyNumberOne[2]);
+// }
+// else
+// {
+//     System.Console.WriteLine($"Третье число отсутствует");
+// }
+
+
+// //Задание 3
+
+// System.Console.Write("Введите число недели -> ");
+// int dayNumber = Convert.ToInt32(Console.ReadLine());
+// void CheckingTheDayOfTheWeek(int dayNumber)
+// {
+//     if(dayNumber == 6 || dayNumber == 7)
+//     {
+//     System.Console.WriteLine($"Это выходной день");
+//     }
+//     else if (dayNumber < 1 || dayNumber > 7)
+//     {
+//     System.Console.WriteLine($"Это не день недели");
+//     }
+//     else
+//     System.Console.WriteLine($"Это будничный день");
+// }
+// CheckingTheDayOfTheWeek(dayNumber);
+
+
